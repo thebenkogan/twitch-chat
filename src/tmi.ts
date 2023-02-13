6c218681eff65fe92d1783842eb6415c8b38de92
thebenkogan <benkogan9@gmail.com>
2023-02-13 01:43:27 -0500

    message limits

--- src/tmi.ts
+++ src/tmi.ts
@@ -1,8 +1,10 @@
 import { createSignal } from "solid-js";
 import tmi from "tmi.js";
 
+const MESSAGE_LIMIT = 100;
+
 const client = new tmi.Client({
-  channels: ["KaiCenat"],
+  channels: ["xQc"],
 });
 
 client.connect();
@@ -16,10 +18,12 @@ function useMessages() {
   const [messages, setMessages] = createSignal<Message[]>([]);
 
   client.on("message", (channel, tags, message, self) => {
-    setMessages((messages) => [
-      ...messages,
-      { user: tags["display-name"]!, message },
-    ]);
+    setMessages((messages) => {
+      const new_messages =
+        messages.length >= MESSAGE_LIMIT ? messages.slice(1) : messages.slice();
+      new_messages.push({ user: tags["display-name"]!, message });
+      return new_messages;
+    });
   });
 
   return messages;
