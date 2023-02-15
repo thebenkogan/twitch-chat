ba137c4fbd03db0210c9747dfd0ddf394564a625
thebenkogan <benkogan9@gmail.com>
2023-02-15 04:00:04 -0500

    home search page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,30 @@
-import { Component } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import { Component, createSignal } from "solid-js";
 
 const Home: Component = () => {
-  return <div>Home!</div>;
+  const navigate = useNavigate();
+  const [search, setSearch] = createSignal("");
+
+  return (
+    <div class="h-screen flex flex-col justify-center items-center text-center">
+      <h1 class="text-3xl sm:text-5xl lg:text-7xl font-bold my-10">
+        Welcome to SolidJS Chat!
+      </h1>
+      <form
+        class="w-full"
+        onSubmit={(e) => {
+          e.preventDefault();
+          navigate(`/${search()}`);
+        }}
+      >
+        <input
+          type="search"
+          class="rounded-xl text-xl p-2 h-10 w-2/3 sm:w-1/2"
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
